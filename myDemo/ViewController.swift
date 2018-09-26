cf89d1e1795ddf7dcd3c6142927871f6a225b503
Dinaol Tadesse <dinaolmelak@gmail.com>
2018-09-26 09:24:09 -0500

    Initial Commit

---
+++ myDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myDemo
+//
+//  Created by Dinaol Melak on 9/26/18.
+//  Copyright © 2018 Dinaol Melak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
