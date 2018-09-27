eeda4357906f9041dee7950deee97107e5fa63bc
Dinaol Tadesse <dinaolmelak@gmail.com>
2018-09-26 21:16:54 -0500

    first commit on myDemo

--- myDemo/ViewController.swift
+++ myDemo/ViewController.swift
@@ -10,11 +10,44 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myTextField: UITextField!
+    var backgroundColor: UIColor!
+    var myLabelColor: UIColor!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        backgroundColor=view.backgroundColor
+        myLabelColor=myLabel.textColor
     }
-
+    @IBAction func didTapAdjustText(_ sender: Any) {
+        myLabel.text=myTextField.text
+        if(myTextField.text=="")
+        {
+            myLabel.text="Hello from Dinaol :)"
+        }
+        myTextField.text=""
+        view.endEditing(true)
+    }
+    @IBAction func didTapChangeTextColor(_ sender: Any) {
+        myLabel.textColor=UIColor.blue
+    }
+    @IBAction func didTapChangeBackColor(_ sender: Any) {
+        view.backgroundColor=UIColor.red
+    }
+    @IBAction func didTapChangeText(_ sender: Any) {
+        myLabel.text="👋 Goodbye!"
+    }
+    @IBAction func didTapReset(_ sender: Any) {
+        myLabel.text="Hello from Dinaol :)"
+        myLabel.textColor=UIColor.white
+        view.backgroundColor=backgroundColor
+        myLabel.textColor=myLabelColor
+    }
+    
+    
+    
 
 }
 
